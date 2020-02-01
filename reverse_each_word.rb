4039b3d2ea5a2e260d5bc1a8873d3db3b90b665a
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 03:05:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   new_sentence = sentence1.split
-  sentence1.each do |reverse|
+  new_sentence.each do |reverse|
     new_sentence << sentence1.reverse
   end
 end
