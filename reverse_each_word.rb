7ca2c8479e288d29e9273aa121d2139076865b81
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 03:27:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,9 @@ def reverse_each_word(sentence1)
   return_array.join(" ")
 end
 
-def reverse_each_word(sentence1)
-  new_sentence = sentence1.split
-  new_sentence.collect do |string|
-    string.reverse
+def reverse_each_word(sentence2)
+  newer_sentence = sentence2.split
+  newer_sentence.collect do |stringling|
+    stringling.reverse
   end
-  return_array.join(" ")
 end
